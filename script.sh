7296e6cc1474c54e99ae881371826bbf7d2718c4
Danila Zaytsev <lfyz20@mail.ru>
2023-12-28 00:57:07 +0300

    maybe worked version

--- script.sh
+++ script.sh
@@ -2,22 +2,22 @@
 
 source_server="localhost"
 target_server=$1
-while source_dir= read -r line;do echo $line; done < service.txt
-target_dir="/opt/$source_dir/app"
+source_dir=$(cat service.txt)
 
-defuser="fitp"
-pass="password"
-
-
-function copy_jars() {
-  for jar_file in $source_dir/*jar; do
-    if [ -f "$jar_file" ]; then
-      scp $source_dir/*.jar $user@$target_server:$target_dir
-    fi
-  done
-}
-
-if ! ssh $user@$source_server "test -d $source_dir"; then
-echo "Source directory $source_dir does not exist on $source_server"
-exit 1
+echo "Введи юзера или нажми enter"
+read -r user
+if [ -z "$user" ]
+then
+  user="fitp"
 fi
+echo "Введи пасс"
+read -r pass
+
+if ! ssh $user@$source_server "test -d \"$source_dir\""; then
+  echo "Source directory $source_dir does not exist on $source_server"
+  exit 1
+fi
+
+for dir in $source_dir; do
+  sshpass -p "$pass" scp "$dir"/*.jar $user@"$target_server":"$dir"
+done
