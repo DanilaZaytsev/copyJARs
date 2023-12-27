9457b2f1286484d44210803f5b21aca9d1429905
DanilaZaytsev <lfyz20@mail.ru>
2023-12-28 00:25:42 +0300

    Что-то сделал, зачем не знаю. просто

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 source_server="localhost"
-target_server=$2
+target_server=$1
 while source_dir= read -r line;do echo $line; done < service.txt
 target_dir="/opt/$source_dir/app"
 
@@ -11,13 +11,13 @@ pass="password"
 
 function copy_jars() {
   for jar_file in $source_dir/*jar; do
-    if [ -f “$jar_file” ]; then
+    if [ -f "$jar_file" ]; then
       scp $source_dir/*.jar $user@$target_server:$target_dir
     fi
   done
 }
 
-if ! ssh $user@$source_server “test -d $source_dir”; then
-echo “Source directory $source_dir does not exist on $source_server”
+if ! ssh $user@$source_server "test -d $source_dir"; then
+echo "Source directory $source_dir does not exist on $source_server"
 exit 1
 fi
